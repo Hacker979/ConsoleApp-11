a9ad083e6ad948c27b107e4494d73a047ef004fa
jotaro4u <kothaker135@gmail.com>
2024-12-02 22:37:04 +0300

    n5

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 namespace ConsoleApp_11
 {
     /// <summary>
-    /// Задача 6. Рассчитать значение х, определив и используя статический метод:
+    /// Задача 5. Рассчитать значение х, определив и используя статический метод:
     /// <param name="args"></param>
     class Program
     {
         static double n6()
         {
-            return (((Math.Sqrt(2) + 2) / (Math.Sqrt(7) + 7)) + ((Math.Sqrt(5) + 5) / (Math.Sqrt(13) + 13)) + ((Math.Sqrt(11) + 11) / (Math.Sqrt(8) + 8)));
+            return (((Math.Sqrt(6) + 6) / 2) + ((Math.Sqrt(13) + 13) / 2) + ((Math.Sqrt(21) + 21) / 2));
         }
         static int input(string a)
         {
