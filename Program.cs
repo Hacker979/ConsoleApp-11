6c7209b03fd835eeae26e36e807049d522986ce6
jotaro4u <kothaker135@gmail.com>
2024-12-02 22:49:55 +0300

    n2

--- Program.cs
+++ Program.cs
@@ -7,25 +7,25 @@ using System.Threading.Tasks;
 namespace ConsoleApp_11
 {
     /// <summary>
-    /// Задача 1. Определить статический метод int Centimeters (int m, int c),
-    /// который принимает два целых аргумента (метры m, сантиметры c) и возвращает количество сантиметров. Используя этот метод,
-    /// вычислить количество сантиметров для длины:
+    /// Задача 2. Определить статический метод int Kop (int r, int k),
+    /// который принимает два целых аргумента (рубли r, копейки k) и возвращает количество копеек.
+    /// Используя этот метод, вычислить количество копеек при:
     /// </summary>
     /// <param name="args"></param>
     class Program
     {
-        static int Meters(int m, int c)
+        static int Kop(int r, int k)
         {
-            return m * 100 + c;
+            return r * 100 + k;
         }
         
         static void Main(string[] args)
         {
-            Console.Write("Введите метры:");
-            int m = int.Parse(Console.ReadLine());
-            Console.Write("Введите сантиметры:");
-            int c = int.Parse(Console.ReadLine());
-            Console.WriteLine(Meters(m,c));
+            Console.Write("Введите количество рублей:");
+            int r = int.Parse(Console.ReadLine());
+            Console.Write("Введите количество копеек:");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine(Kop(r,k));
             Console.Read();
         }
     }
