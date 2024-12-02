40ba298d95dfc1db5fe2c4934844a4f0f9cb9176
jotaro4u <kothaker135@gmail.com>
2024-12-03 00:01:26 +0300

    n4

--- Program.cs
+++ Program.cs
@@ -7,34 +7,30 @@ using System.Threading.Tasks;
 namespace ConsoleApp_11
 {
     /// <summary>
-    /// Задача 3. Найти минимальное из трех целых чисел, используя вспомогательные методы.
+    /// Задача 4. Рассчитать значение z=min(2a,b+a)+min(2a-b,b),
+    /// определив и используя функцию Min(x,y) – минимальное из двух чисел.
     /// <summary>
     /// <param name="args"></param>
     class Program
     {
-        static int Min2(int x, int y)
+        static int Min(int x, int y)
         {
             return x < y ? x : y;
         }
 
-        static int Min3(int a, int b, int c)
-        {
-            return Min2(Min2(a, b), c);
-        }
-
-        static int input(string a)
+        static int Input(string a)
         {
             Console.Write(a);
             return int.Parse(Console.ReadLine());
         }
-        
+
         static void Main(string[] args)
         {
-            int a = input("Введите a: ");
-            int b = input("Введите b: ");
-            int c = input("Введите c: ");
-            Console.WriteLine($"ответ:{Min3(a,b,c)}");
-            Console.Read();
+            int a = Input("Введите a: ");
+            int b = Input("Введите b: ");
+            int z = Min(2 * a, b + a) + Min(2 * a - b, b);
+            Console.WriteLine($"Ответ: {z}");
+            Console.ReadLine();
         }
     }
 }
