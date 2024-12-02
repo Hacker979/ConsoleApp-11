fee1dd8cc9afba8505cf456bdcb3f845305113ff
jotaro4u <kothaker135@gmail.com>
2024-12-02 22:42:09 +0300

    n1

--- Program.cs
+++ Program.cs
@@ -7,25 +7,25 @@ using System.Threading.Tasks;
 namespace ConsoleApp_11
 {
     /// <summary>
-    /// Задача 1. Определить статический метод int Meters (int k, int m),
-    /// который принимает два целых аргумента (километры k, метры m) и возвращает количество метров.
-    /// Используя этот метод, вычислить количество метров для расстояния:
+    /// Задача 1. Определить статический метод int Centimeters (int m, int c),
+    /// который принимает два целых аргумента (метры m, сантиметры c) и возвращает количество сантиметров. Используя этот метод,
+    /// вычислить количество сантиметров для длины:
     /// </summary>
     /// <param name="args"></param>
     class Program
     {
-        static int Meters(int k, int m)
+        static int Meters(int m, int c)
         {
-            return k * 1000 + m;
+            return m * 100 + c;
         }
         
         static void Main(string[] args)
         {
-            Console.Write("Введите километры:");
-            int k = int.Parse(Console.ReadLine());
             Console.Write("Введите метры:");
             int m = int.Parse(Console.ReadLine());
-            Console.WriteLine(Meters(k,m));
+            Console.Write("Введите сантиметры:");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(Meters(m,c));
             Console.Read();
         }
     }
